e13f73b74d044d44163a3a6f0e3ad801529c8818
Kevin Altman <altmank@gmail.com>
2022-06-11 03:42:11 -0500

    Add new terrain editing cheats

---
+++ src/Cheats/Terrain/TerrainCheatProvider.cs
@@ -0,0 +1,138 @@
+﻿using System.Linq;
+using Mafi;
+using Mafi.Base;
+using Mafi.Core;
+using Mafi.Core.Products;
+using Mafi.Core.Prototypes;
+using Mafi.Core.Terrain;
+using Mafi.Core.Terrain.Designation;
+using Mafi.Core.Terrain.Trees;
+
+namespace CaptainOfCheats.Cheats.Terrain
+{
+    public class TerrainCheatProvider
+    {
+        private readonly ProtosDb _protosDb;
+        private readonly ITerrainDesignationsManager _terrainDesignationsManager;
+        private readonly ITerrainDumpingManager _terrainDumpingManager;
+        private readonly ITerrainMiningManager _terrainMiningManager;
+        private readonly TreeManager _treeManager;
+
+        public void AddTree()
+        {
+            
+        }
+        
+        public void RemoveAllSelectedTrees()
+        {
+            var treeIds = _treeManager.EnumerateSelectedTrees().ToList();
+
+            for (var index = 0; index < treeIds.Count; index++)
+            {
+                var treeId = treeIds[index];
+                _treeManager.TryRemoveTree(treeId);
+            }
+        }
+
+        public void HarvestTreesInTerrainDesignation(TerrainDesignation designation)
+        {
+            var designationArea = designation.Area;
+
+            var treesInArea = _treeManager.EnumerateTreesInArea(designationArea);
+
+            var treeIds = treesInArea.ToList();
+
+            for (var index = 0; index < treeIds.Count; index++)
+            {
+                var treeId = treeIds[index];
+                _treeManager.TryRemoveTree(treeId);
+            }
+        }
+        
+        public TerrainCheatProvider(ITerrainDesignationsManager terrainDesignationsManager, ProtosDb protosDb, 
+            ITerrainDumpingManager terrainDumpingManager, ITerrainMiningManager terrainMiningManager, TreeManager treeManager)
+        {
+            _terrainDesignationsManager = terrainDesignationsManager;
+            _protosDb = protosDb;
+            _terrainDumpingManager = terrainDumpingManager;
+            _terrainMiningManager = terrainMiningManager;
+            _treeManager = treeManager;
+        }
+
+        public void CompleteAllMiningDesignations(bool doNoTerrainPhysics = false)
+        {
+            var miningTerrainDesignations = _terrainMiningManager.MiningDesignations.Where(x => x.IsNotFulfilled);
+
+            foreach (var designation in miningTerrainDesignations)
+            {
+                HarvestTreesInTerrainDesignation(designation);
+                SetHeightToMatchDesignation(designation, doNoTerrainPhysics);
+            }
+        }
+
+        public void CompleteAllDumpingDesignations(ProductProto.ID looseMaterialProductId, bool doNoTerrainPhysics = false)
+        {
+            var looseProductProto = _protosDb.First<LooseProductProto>(x => x.Id == looseMaterialProductId);
+
+            var dumpingDesignations = _terrainDumpingManager.DumpingDesignations
+                .Where(x => x.IsNotFulfilled);
+
+            foreach (var designation in dumpingDesignations)
+            {
+                HarvestTreesInTerrainDesignation(designation);
+                ChangeMaterial(designation, looseProductProto.Value);
+                SetHeightToMatchDesignation(designation, doNoTerrainPhysics);
+            }
+        }
+
+        public void ChangeMaterial(TerrainDesignation terrainDesignation, LooseProductProto newTerrainMaterial)
+        {
+            var looseProductQuantity = new LooseProductQuantity(newTerrainMaterial, Quantity.MaxValue);
+            var terrainThickness = looseProductQuantity.ToTerrainThickness();
+
+            terrainDesignation.ForEachTile((tile, f) => { DumpTile(tile, ref terrainThickness, ThicknessTilesF.MaxValue, terrainDesignation, true); });
+        }
+
+        public void SetHeightToMatchDesignation(TerrainDesignation terrainDesignation, bool doNoTerrainPhysics)
+        {
+            terrainDesignation.ForEachTile((tile, f) => { tile.SetHeight(GetTargetHeight(terrainDesignation, tile.TileCoord), doNoTerrainPhysics: doNoTerrainPhysics); });
+        }
+
+        public void DepositUpToHeight(TerrainDesignation terrainDesignation)
+        {
+            var designations = _terrainDesignationsManager.Designations.Where(x => x.IsNotFulfilled);
+            var rock = _protosDb.First<LooseProductProto>(x => x.Id == Ids.Products.Rock);
+
+            foreach (var designation in designations)
+            {
+                var looseProductQuantity = new LooseProductQuantity(rock.Value, Quantity.MaxValue);
+                var terrainThickness = looseProductQuantity.ToTerrainThickness();
+
+                designation.ForEachTile((tile, f) => { tile.DepositLooseProductUpToHeight(terrainThickness, GetTargetHeight(terrainDesignation, tile.TileCoord), ThicknessTilesF.MaxValue); });
+            }
+        }
+
+
+        private HeightTilesF GetTargetHeight(TerrainDesignation terrainDesignation, Tile2i position)
+        {
+            if (terrainDesignation.ContainsPosition(position)) return terrainDesignation.GetTargetHeightAt(position);
+
+            var dumpingDesignationAt = _terrainDumpingManager.GetDumpingDesignationAt(position);
+            return dumpingDesignationAt.HasValue ? dumpingDesignationAt.Value.GetTargetHeightAt(position) : HeightTilesF.MinValue;
+        }
+
+        private void DumpTile(TerrainTile tile, ref TerrainMaterialThickness product, ThicknessTilesF maxDumped, TerrainDesignation terrainDesignation, bool doNotChangeHeight = false,
+            bool doNotRaiseEvents = false,
+            bool doNotDisruptTerrain = false)
+        {
+            var targetHeight = GetTargetHeight(terrainDesignation, tile.TileCoord);
+            if (tile.Height >= targetHeight) return;
+
+            var thicknessTilesF = targetHeight - tile.Height;
+            thicknessTilesF = thicknessTilesF.Min(maxDumped);
+            var thickness = thicknessTilesF.Min(product.Thickness);
+            tile.DepositMaterial(product.Material, thickness, doNotRaiseEvents: doNotRaiseEvents, doNotDisruptTerrain: doNotDisruptTerrain, doNotChangeHeight: doNotChangeHeight);
+            product = new TerrainMaterialThickness(product.Material, product.Thickness - thickness);
+        }
+    }
+}
---
+++ src/Cheats/Terrain/TerrainCheatTab.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Linq;
+using Mafi;
+using Mafi.Base;
+using Mafi.Collections;
+using Mafi.Core;
+using Mafi.Core.Products;
+using Mafi.Core.Prototypes;
+using Mafi.Core.Syncers;
+using Mafi.Localization;
+using Mafi.Unity.InputControl;
+using Mafi.Unity.UiFramework;
+using Mafi.Unity.UiFramework.Components;
+using Mafi.Unity.UiFramework.Components.Tabs;
+using UnityEngine;
+using Assets = Mafi.Unity.Assets;
+
+namespace CaptainOfCheats.Cheats.Terrain
+{
+    [GlobalDependency(RegistrationMode.AsEverything)]
+    public class TerrainCheatTab : Tab, ICheatProviderTab
+    {
+        private readonly ProtosDb _protosDb;
+        private readonly TerrainCheatProvider _cheatProvider;
+        private bool _disableTerrainPhysicsOnMiningAndDumping = true;
+        private readonly Dict<SwitchBtn, Func<bool>> _switchBtns = new Dict<SwitchBtn, Func<bool>>();
+        private readonly IOrderedEnumerable<LooseProductProto> _looseProductProtos;
+        private ProductProto.ID? _selectedLooseProductProto;
+
+        public TerrainCheatTab(NewInstanceOf<TerrainCheatProvider> cheatProvider, ProtosDb _protosDb
+        ) : base(nameof(TerrainCheatTab), SyncFrequency.OncePerSec)
+        {
+            this._protosDb = _protosDb;
+            _cheatProvider = cheatProvider.Instance;
+            
+            _looseProductProtos = _protosDb.Filter<LooseProductProto>(proto => proto.CanBeLoadedOnTruck && proto.CanBeOnTerrain).OrderBy(x => x);
+        }
+
+        public string Name => "Terrain";
+        public string IconPath => Assets.Unity.UserInterface.Toolbar.Dumping_svg;
+
+        protected override void BuildUi()
+        {
+            var topOf = CreateStackContainer();
+
+            var terrainSelector = BuildTerrainSelector(topOf);
+            var terrainPhysicsToggleSwitch = CreateTerrainPhysicsToggleSwitch();
+            terrainPhysicsToggleSwitch.PutToRightOf(terrainSelector, terrainPhysicsToggleSwitch.GetWidth(),Offset.Right(-200f));
+            
+            BuildMineButton(topOf);
+            BuildDumpButton(topOf);
+            BuildRemoveTreesButton(topOf);
+        }
+
+        private Dropdwn BuildTerrainSelector(StackContainer topOf)
+        {
+            Builder
+                .AddSectionTitle(topOf, new LocStrFormatted("Terrain"), new LocStrFormatted("Select the terrain to use when dumping."));
+            
+            var productDropdown = Builder
+                .NewDropdown("TerrainDumpSelector")
+                .AddOptions(_looseProductProtos.Select(x => x.Id.ToString().Replace("Product_", "")).ToList())
+                .OnValueChange(i => _selectedLooseProductProto = (ProductProto.ID)_looseProductProtos.ElementAt(i)?.Id)
+                .AppendTo(topOf, new Vector2(150, 28f), ContainerPosition.LeftOrTop);
+
+            _selectedLooseProductProto = _looseProductProtos.ElementAt(0)?.Id;
+
+            return productDropdown;
+
+        }
+        
+        private SwitchBtn CreateTerrainPhysicsToggleSwitch()
+        {
+            var toggleBtn = Builder.NewSwitchBtn()
+                .SetText("Disable Terrain Physics")
+                .AddTooltip("When instantly completing mining or dumping designations, this toggle will indicate whether or not the game physics engine will affect the modified terrain. When turned on, expect very sharp edges on any terrain modifications you make. Note: Vehicles mining/dumping near no-physics terrain may cause no-physics terrain to start responding to physics.")
+                .SetOnToggleAction((toggleVal) => _disableTerrainPhysicsOnMiningAndDumping = toggleVal );
+
+            
+            _switchBtns.Add(toggleBtn, () => _disableTerrainPhysicsOnMiningAndDumping);
+
+            return toggleBtn;
+        }
+        
+        private StackContainer CreateStackContainer()
+        {
+            var topOf = Builder
+                .NewStackContainer("container")
+                .SetStackingDirection(StackContainer.Direction.TopToBottom)
+                .SetSizeMode(StackContainer.SizeMode.Dynamic)
+                .SetInnerPadding(Offset.All(15f))
+                .SetItemSpacing(5f)
+                .PutToTopOf(this, 0.0f);
+            return topOf;
+        }
+        
+        private void BuildMineButton(StackContainer topOf)
+        {
+            var spawnProductBtn = Builder.NewBtn("button")
+                .SetButtonStyle(Style.Global.PrimaryBtn)
+                .SetText(new LocStrFormatted("Instantly Complete Mining Designations"))
+                .AddToolTip("All areas currently designated for mining will have their dump operation completed immediately. Results in no resources for the player. WARNING: If terrain physics is turned on, be aware that large mining operations can take awhile to finish due to physics catching up.")
+                .OnClick(() => _cheatProvider.CompleteAllMiningDesignations(_disableTerrainPhysicsOnMiningAndDumping));
+
+            spawnProductBtn.AppendTo(topOf, spawnProductBtn.GetOptimalSize(), ContainerPosition.LeftOrTop, Offset.Top(10f));
+        }
+
+        private void BuildDumpButton(StackContainer topOf)
+        {
+            var spawnProductBtn = Builder.NewBtn("button")
+                .SetButtonStyle(Style.Global.PrimaryBtn)
+                .SetText(new LocStrFormatted("Instantly Complete Dumping Designations"))
+                .AddToolTip("All areas currently designated for dumping will have their dump operation completed immediately. Requires no resources from the player. If terrain physics is turned on, the shape you create will be altered by terrain physics after the material spawns in.")
+                .OnClick(() => _cheatProvider.CompleteAllDumpingDesignations((ProductProto.ID)_selectedLooseProductProto, _disableTerrainPhysicsOnMiningAndDumping));
+
+            spawnProductBtn.AppendTo(topOf, spawnProductBtn.GetOptimalSize(), ContainerPosition.LeftOrTop, Offset.Top(10f));
+        }
+        
+        private void BuildRemoveTreesButton(StackContainer topOf)
+        {
+            var spawnProductBtn = Builder.NewBtn("button")
+                .SetButtonStyle(Style.Global.PrimaryBtn)
+                .SetText(new LocStrFormatted("Remove All Selected Trees"))
+                .OnClick(() => _cheatProvider.RemoveAllSelectedTrees());
+
+            spawnProductBtn.AppendTo(topOf, spawnProductBtn.GetOptimalSize(), ContainerPosition.LeftOrTop, Offset.Top(10f));
+        }
+        
+        
+        public override void RenderUpdate(GameTime gameTime)
+        {
+            RefreshValues();
+            base.RenderUpdate(gameTime);
+        }
+
+        public override void SyncUpdate(GameTime gameTime)
+        {
+            RefreshValues();
+            base.SyncUpdate(gameTime);
+        }
+
+        private void RefreshValues()
+        {
+            foreach (var kvp in _switchBtns) kvp.Key.SetState(kvp.Value());
+        }
+
+        
+        
+    }
+}
